72c371c48666a1b82bd1f761c5ab189507e020bb
Spider <29214314+spide-r@users.noreply.github.com>
2024-12-01 16:30:10 -0600

    bigboy - day 1

--- src/Day1.java
+++ src/Day1.java
@@ -1,8 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Day1 {
 
@@ -10,7 +12,7 @@ public class Day1 {
         ArrayList<Integer> left = new ArrayList<>();
         ArrayList<Integer> right = new ArrayList<>();
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("Day1.txt"));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("bigboys\\Day1.txt"));
             bufferedReader.lines().forEach(line ->
             {
                 String[] parts = line.split(" {3}");
@@ -20,9 +22,9 @@ public class Day1 {
             });
             left.sort(Comparator.naturalOrder());
             right.sort(Comparator.naturalOrder());
-            int distance = partOne(left, right);
+            long distance = partOneV2(left, right);
             System.out.println("Final Distance is " + distance);
-            int score = partTwo(left, right);
+            long score = partTwoV2(left, right);
 
             System.out.println("Final Score is " + score);
         } catch (FileNotFoundException e) {
@@ -41,8 +43,36 @@ public class Day1 {
         return score;
     }
 
-    public static int partOne(ArrayList<Integer> left,ArrayList<Integer> right ){
+    public static long partTwoV2(ArrayList<Integer> left, ArrayList<Integer> right){
+        long score = 0L;
+
+        int leftIdx = 0;
+        for (int rightInt : right) {
+            int leftInt = left.get(leftIdx);
+            while (leftInt < rightInt) {
+                leftIdx++;
+                leftInt = left.get(leftIdx);
+            }
+            if (leftInt == rightInt) {
+                score += rightInt;
+            }
+        }
+        return score;
+    }
+
+
+    public static int partOne(ArrayList<Integer> left,ArrayList<Integer> right){
         int distance = 0;
+        for (int i = 0; i < left.size(); i++) {
+            int leftInt = left.get(i);
+            int rightInt = right.get(i);
+            int diff = Math.abs(leftInt - rightInt);
+            distance += diff;
+        }
+        return distance;
+    }
+    public static long partOneV2(ArrayList<Integer> left,ArrayList<Integer> right ){
+        long distance = 0L;
         for (int i = 0; i < left.size(); i++) {
             int leftInt = left.get(i);
             int rightInt = right.get(i);
