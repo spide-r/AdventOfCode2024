7ea2616f4dc09a82f3cdfe9d72e5796a1e2fe8e4
Spider <29214314+spide-r@users.noreply.github.com>
2024-12-02 23:14:51 -0600

    day 3 - part 2

--- src/Day3.java
+++ src/Day3.java
@@ -11,15 +11,27 @@ public class Day3 {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("input\\Day3.txt"));
             final int[] finalAmt = {0};
-            Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)");
+            Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)|do(n't)?\\(\\)");
+            final boolean[] enabled = {true};
             bufferedReader.lines().forEach(line ->
             {
                 Matcher matcher = pattern.matcher(line);
                 while (matcher.find()){
                     String found = matcher.group();
-                    int first = Integer.parseInt(found.substring(4, found.indexOf(',')));
-                    int second = Integer.parseInt(found.substring(found.indexOf(',') + 1, found.indexOf(")")));
-                    finalAmt[0] += first * second;
+                    if(found.startsWith("mul") && enabled[0]){
+                        int first = Integer.parseInt(found.substring(4, found.indexOf(',')));
+                        int second = Integer.parseInt(found.substring(found.indexOf(',') + 1, found.indexOf(")")));
+                        finalAmt[0] += first * second;
+                    }
+
+                    if(found.startsWith("don't")){
+                        enabled[0] = false;
+                    }
+
+                    if(found.startsWith("do(")){
+                        enabled[0] = true;
+                    }
+
                 }
             });
             System.out.println("Final Amount is " + finalAmt[0]);
