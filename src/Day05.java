0059b75aefef108033a93d47e0b635b5b0037b61
Spider <29214314+spide-r@users.noreply.github.com>
2024-12-05 08:07:33 -0600

    day 5 - part 2

--- src/Day05.java
+++ src/Day05.java
@@ -1,22 +1,28 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Day05 {
     public static void main(String[] args) {
         try{
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("input\\Day5Example.txt"));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("input\\Day5.txt"));
+            HashMap<String, HashSet<String>> printRulesInverted = new HashMap<>();
             HashMap<String, HashSet<String>> printRules = new HashMap<>();
             ArrayList<ArrayList<String>> printUpdates = new ArrayList<>();
             bufferedReader.lines().forEach(line -> {
                 if(line.contains("|")){
                     String[] ll = line.split("\\|");
-                    String key = ll[1];
-                    String value = ll[0];
+                    String value = ll[1];
+                    String key = ll[0];
+                    printRulesInverted.compute(value, (k, v) -> {
+                        if(v == null){
+                            v = new HashSet<>();
+                        }
+                        v.add(key);
+                        return v;
+                    });
+
                     printRules.compute(key, (k, v) -> {
                         if(v == null){
                             v = new HashSet<>();
@@ -29,15 +35,17 @@ public class Day05 {
                 }
             });
             int total = 0;
+            int badTotal = 0;
             for (ArrayList<String> printUpdate : printUpdates) {
                 ArrayList<String> disallowed = new ArrayList<>();
                 boolean success = true;
                 for (String s : printUpdate) {
                     if(disallowed.contains(s)){
                         success = false;
+                        break;
                     }
-                    if(printRules.get(s) != null){
-                        disallowed.addAll(printRules.get(s));
+                    if(printRulesInverted.get(s) != null){
+                        disallowed.addAll(printRulesInverted.get(s));
                     }
                 }
 
@@ -45,11 +53,14 @@ public class Day05 {
                     int toAdd = Integer.parseInt(printUpdate.get(printUpdate.size()/2));
                     total += toAdd;
                 } else {
-
+                    ArrayList<String> sortedBadValue = customQuicksort(printUpdate, printRules, printRulesInverted);
+                    int badValue = Integer.parseInt(sortedBadValue.get(sortedBadValue.size()/2));
+                    badTotal += badValue;
                 }
             }
 
             System.out.println(total);
+            System.out.println(badTotal);
 
 
         } catch (Exception e){
@@ -57,5 +68,35 @@ public class Day05 {
         }
     }
 
+    public static ArrayList<String> customQuicksort(ArrayList<String> list, HashMap<String, HashSet<String>> sortingRules, HashMap<String, HashSet<String>> sortingRulesInverted){
+        if(list.size() < 2){
+            return list;
+        }
 
+        int pivotIdx = 0; //bad choice of pivot!!! maybe change?
+        String pivot = list.get(pivotIdx);
+        HashSet<String> rulesForPivot = sortingRules.getOrDefault(pivot, new HashSet<>()); // greater than - pivot must be placed *before* these numbers
+        HashSet<String> rulesForPivotInverse = sortingRulesInverted.getOrDefault(pivot, new HashSet<>()); //less than - pivot must be placed *after* these numbers
+
+        ArrayList<String> before = new ArrayList<>();
+        ArrayList<String> middle = new ArrayList<>();
+        ArrayList<String> after = new ArrayList<>();
+        for (String value : list){
+            if(rulesForPivot.contains(value)){
+                after.add(value);
+            } else if(rulesForPivotInverse.contains(value)){
+                before.add(value);
+            } else {
+                middle.add(value);
+            }
+        }
+
+        ArrayList<String> sortedList = new ArrayList<>();
+        sortedList.addAll(customQuicksort(before, sortingRules, sortingRulesInverted));
+        sortedList.addAll(customQuicksort(middle, sortingRules, sortingRulesInverted));
+        sortedList.addAll(customQuicksort(after, sortingRules, sortingRulesInverted));
+
+
+        return sortedList;
+    }
 }
